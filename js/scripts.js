e10b9b6ec34898d7a3fe1191e314ea8a03d04952
daniel munger and taylor loftis-kim <daniel@>
2016-10-19 16:41:01 -0700

    add bootstrap styling to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-$(document).ready(function(event) {
-  event.preventDefault;
+$(document).ready(function() {
+
   $("form#name").submit(function(event){
+    event.preventDefault();
     var name = $("input#name1").val();
     $("#output-name").text(name);
   });
   $("form#email").submit(function(event){
+    event.preventDefault();
     var email = $("input#email1").val();
     $("#output-email").text(email);
   });
   $("form#address").submit(function(event){
+    event.preventDefault();
     var address = $("input#address1").val();
     $("#output-address").text(address);
   });
